8d27cfa8a8dcb0a7125cd12c22c1d1426a50e742
ShivaniAnandan <124684724+ShivaniAnandan@users.noreply.github.com>
2023-04-02 20:46:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+function sayHello() {
+	return new Promise((resolve) => {
+		setTimeout(() => {
+			resolve("Hello, world!");
+		} , 1000);
+	});
+}
+const output = document.getElementById("output");
+sayHello().then((result) => {
+	output.textContent = result;
+});
